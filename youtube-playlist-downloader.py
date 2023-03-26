99292f7634657ed289379a081c3523e5a7805fa5
Huzi <43390871+HuzaifaPatel@users.noreply.github.com>
2023-03-26 04:24:40 -0400

    final touches

--- youtube-playlist-downloader.py
+++ youtube-playlist-downloader.py
@@ -1,26 +1,52 @@
 from youtube_connecter import youtube, playlist_id
+import os
+import youtube_dl
 
 def get_playlist(token_offset):
 
 	return youtube().playlistItems().list (	
 															
-																part="snippet", 
-																id=None, 
-																maxResults=1, 
-																onBehalfOfContentOwner=None, 
-																pageToken=token_offset, 
-																playlistId=playlist_id, 
-																videoId=None, 
-																x__xgafv=None
-															
-															).execute()
+	part="snippet", 
+	id=None, 
+	maxResults=50, 
+	onBehalfOfContentOwner=None, 
+	pageToken=token_offset, 
+	playlistId=playlist_id, 
+	videoId=None, 
+	x__xgafv=None
+						
+	).execute()
 
 
 def get_youtube_video_id():
 	playlist_data = get_playlist(None)
 
 	while 'nextPageToken' in playlist_data:
-		print("https://www.youtube.com/watch?v=" + playlist_data['items'][0]['snippet']['resourceId']['videoId'])
+		for item in playlist_data['items']:
+			download_video_mp3("https://www.youtube.com/watch?v=" + item['snippet']['resourceId']['videoId'])
 		playlist_data = get_playlist(playlist_data['nextPageToken'])
+
+
+def download_video_mp3(youtube_url):
+	os.chdir("/home/huzi/Music")
+
+	try:
+		ydl_opts = {
+			'outtmpl': '%(title)s.%(ext)s',
+			'noplaylist' : True, 
+			'format' : 'bestaudio/best',
+			'keepvideo': False,
+			'postprocessors': [{
+	         'key': 'FFmpegExtractAudio',
+	         'preferredcodec': 'mp3',
+	         'preferredquality': '192',
+	     }]
+		}
+
+		with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+			ydl.download([youtube_url])
+	except Exception as e:
+		with open("fail.log", "a+") as fail_log:
+			fail_log.write(youtube_url + "\n")
 
 get_youtube_video_id()
--- youtube_connecter.py
+++ youtube_connecter.py
@@ -1,8 +1,8 @@
 from googleapiclient.discovery import build
-SERVICE_NAME	='youtube'
-VERSION 		='v3'
-API_KEY 		='AIzaSyBvpL_mFt3n6_i2AtgXVxCLCz7qj3vlzEc'
-playlist_id     ='PL-iwWIXCptDxMMTT7MOQBAPoLDeFukrsd'
+SERVICE_NAME='youtube'
+VERSION='v3'
+API_KEY='AIzaSyBvpL_mFt3n6_i2AtgXVxCLCz7qj3vlzEc'
+playlist_id='PL-iwWIXCptDyWB9hg2c_qamBhbAmhFw7d'
 
 def youtube():
 	return build(SERVICE_NAME, VERSION, developerKey=API_KEY)
