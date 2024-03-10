69af4a5f5c57574a4cd9588b6595aad9cb222332
huzi <huzaifa593@hotmail.com>
2024-03-10 05:59:12 -0400

    remove api key

--- youtube_connecter.py
+++ youtube_connecter.py
@@ -1,9 +1,12 @@
 from googleapiclient.discovery import build
 SERVICE_NAME='youtube'
 VERSION='v3'
-API_KEY='AIzaSyByaGARKb5XBFPvuc4nl_3B9TfoJDQxkZw'
 playlist_id='PL-iwWIXCptDxMMTT7MOQBAPoLDeFukrsd'
 
+def get_api_key():
+	api_key = open("api_key", "r")
+	return api_key.read()
+
 def youtube():
-	return build(SERVICE_NAME, VERSION, developerKey=API_KEY)
+	return build(SERVICE_NAME, VERSION, developerKey=get_api_key())
 	
